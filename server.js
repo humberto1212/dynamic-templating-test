81b3d3cdce700c358bbca94490ddd685d536d28a
Humberto Angelucci <angeluccirincon@gmail.com>
2021-06-23 16:44:04 +0200

    Done

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const hbs = require('hbs');
 
+//reqire students
+const students = require('./exam-info')
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -12,13 +15,33 @@ app.use(express.static('public'));
 
 // 1: in the home,list all the students who took the exam (list all the students)
 
-// ... Your code here
+app.get('/', (req, res) => {
+  res.render('full-list.hbs', { students })
+})
 
 // 2: in the '/results' list all the students who passed the test and their score.
 // Also, students should be in descending order based on their score.
 
-// ... Your code here
+app.get('/results', (req, res) => {
+
+let cloneStudents = JSON.parse(JSON.stringify(students))
+
+  
+
+  let passed = cloneStudents.filter((note) => {
+   return note.hasPassed === true
+  })
+
+  .sort((a,b) => b.score - a.score)
+
+
+
+  res.render('results.hbs', { passed })
+})
+
+
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on ${process.env.PORT}.`)
 );
+
